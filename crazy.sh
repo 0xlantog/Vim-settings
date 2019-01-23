b13207e05541006f13ca8121db5e0ad3f581f4f8
Lucas Antognini <36196629+0xlantog@users.noreply.github.com>
2019-01-23 19:25:23 -0300

    Crazinest
    asdf

---
+++ crazy.sh
@@ -0,0 +1,4 @@
+sudo apt install vim git
+sudo apt-get install git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev dh-autoreconf 
+https://github.com/Airblader/xcb-util-xrm 
+cd xcb-util-xrm/ ./autogen.sh make sudo make install
