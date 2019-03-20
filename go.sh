28ca1a18e22866a0ea21eeedd09a4c3babde4ad6
Lucas Antog <lantog@protonmail.com>
2019-03-20 15:48:05 -0300

    asdf

--- go.sh
+++ go.sh
@@ -11,16 +11,22 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Fonts
 sudo apt install fonts-roboto fonts-inconsolata -y
+sudo mkdir -p /usr/share/fonts/truetype/ttf-monaco
+cd /usr/share/fonts/truetype/ttf-monaco
+sudo wget http://www.gringod.com/wp-upload/software/Fonts/Monaco_Linux.ttf
+sudo mkfontdir
+cd ..
+fc-cache
 
 sudo chmod 1777 /opt/ && cd /opt/
 
 # Gaps
-sudo apt install git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev \
-libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
-libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y \
-&& git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps && autoreconf --force --install \
-&& rm -rf build/ && mkdir -p build && cd build/ && ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers \
-&& make && sudo make install
+# sudo apt install git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev \
+# libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+# libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y \
+# && git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps && autoreconf --force --install \
+# && rm -rf build/ && mkdir -p build && cd build/ && ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers \
+# && make && sudo make install
 
 cd $HOME
 
@@ -55,8 +61,8 @@ curl -o ~/.Xdefaults https://raw.githubusercontent.com/0xlantog/terminal-tools-s
 # Compton
 curl -o ~/.config/compton.conf https://raw.githubusercontent.com/0xlantog/terminal-tools-settings/master/compton.conf
 
-cd /tmp && wget https://github.com/FortAwesome/Font-Awesome/releases/download/5.7.2/fontawesome-free-5.7.2-web.zip
-unzip fontawesome-free-5.7.2-web.zip && cd fontawesome-free-5.7.2-web/webfonts
-mkdir $HOME/.fonts && cp *.ttf $HOME/.fonts
+#cd /tmp && wget https://github.com/FortAwesome/Font-Awesome/releases/download/5.7.2/fontawesome-free-5.7.2-web.zip
+#unzip fontawesome-free-5.7.2-web.zip && cd fontawesome-free-5.7.2-web/webfonts
+#mkdir $HOME/.fonts && cp *.ttf $HOME/.fonts
 
 cd $HOME
